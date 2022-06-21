fb4d0691ac2c453bf20ab905042a2df0c707cf76
Long0311 <72438650+nguyenthanhlong031120@users.noreply.github.com>
2022-06-21 22:03:23 +0700

    Revert "Update Admin Dashboard"
    This reverts commit ac9a4571ddc3915589439617503a4a1192002d13.

--- client/src/page/AdminMovieList/index.jsx
+++ client/src/page/AdminMovieList/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { Button, Table } from "react-bootstrap"
+import {Button, Table} from "react-bootstrap"
 import { useDispatch, useSelector } from 'react-redux';
-import { adminActions } from "../../redux-toolkit/slice/admin"
+import {adminActions} from "../../redux-toolkit/slice/admin"
 
 function MovieList(props) {
     const dispatch = useDispatch()
     const movieList = useSelector(state => state.admin.movielist);
     const genres = useSelector(state => state.public.genres);
     const [show, setShow] = useState(false);
-    useEffect(() => {
+    useEffect( () => {
         dispatch(adminActions.fetchMovieList());
-    }, [])
+    },[] )
 
     const handleDeleteMovie = (e) => {
 
@@ -29,80 +29,82 @@ function MovieList(props) {
     }
     return (
         <Table striped responsive="lg" bordered hover variant="dark">
-            <thead>
-                <tr fixed>
+        <thead>
+          <tr fixed>
 
-                    <th>STT</th>
-                    <th>ID</th>
-                    <th>Tên phim</th>
-                    <th>Tên khác</th>
-                    <th>Tên phim URL</th>
-                    <th>Thời lượng</th>
-                    <th>Quốc gia</th>
-                    <th>Ngôn ngữ</th>
-                    <th>Đạo diễn</th>
-                    <th>Năm</th>
-                    <th>Loại Phim</th>
-                    <th>Thể loại</th>
-                    <th>Lượt  xem</th>
-                    <th style={{ minWidth: "300px" }}>Mô tả</th>
-                    <th>Ngày tạo</th>
-                    <th style={{ minWidth: "150px" }}>Chỉnh sửa</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    movieList?.map((movie, index) =>
-                        <tr key={index}>
+            <th>STT</th>
+            <th>ID</th>
+            <th>Tên phim</th>
+            <th>Tên khác</th>
+            <th>Tên phim URL</th>
+            <th>Thời lượng</th>
+            <th>Quốc gia</th>
+            <th>Ngôn ngữ</th>
+            <th>Đạo diễn</th>
+            <th>Năm</th>
+            <th>Loại Phim</th>
+            <th>Thể loại</th>
+            <th>Lượt  xem</th>
+            <th style={{minWidth: "300px"}}>Mô tả</th>
+            <th>Ngày tạo</th>
+            <th style={{minWidth: "150px"}}>Chỉnh sửa</th>
+          </tr>
+        </thead>
+        <tbody>
+            {
+                movieList?.map((movie, index) => 
+                    <tr  key={index}>
 
-                            <td>{index}</td>
-                            <td>{movie._id}</td>
-                            <td>{movie.name}</td>
-                            <td>{movie.other_name}</td>
-                            <td>{movie.name_URL}</td>
-                            <td>{movie.duration}</td>
-                            <td>{movie.language}</td>
-                            <td>{movie.country}</td>
-                            <td>{movie.director}</td>
-                            <td>{movie.year}</td>
-                            <td>{(movie.type_movie === "phimle") ? "Phim lẻ" : "Phim bộ"}</td>
-                            <td>
-                                <table>
-                                    {movie.genres?.map((item, index) =>
-                                        //  <tr key={index}>
+                        <td>{index}</td>
+                        <td>{movie._id}</td>
+                        <td>{movie.name}</td>
+                        <td>{movie.other_name}</td>
+                        <td>{movie.name_URL}</td>
+                        <td>{movie.duration}</td>
+                        <td>{movie.language}</td>
+                        <td>{movie.country}</td>
+                        <td>{movie.director}</td>
+                        <td>{movie.year}</td>
+                        <td>{(movie.type_movie ==="phimle")?"Phim lẻ" : "Phim bộ"}</td>
+                        <td>
+                            <table>
 
-                                        genres?.map((genre) => {
-                                            if (genre._id === item)
-                                                return `${genre.name} ,`
-                                        })
+                             {movie.genres?.map((item, index) => 
+                            //  <tr key={index}>
+                                
+                                    genres?.map((genre) => {
+                                        if (genre._id === item)
+                                             return `${genre.name } ,`
+                                    })
+                                
+                                // </tr>
+                            )}
+                            </table>
 
-                                        // </tr>
-                                    )}
-                                </table>
+                        </td>
+                        <td>{movie.views}</td>
+                        <td>{movie.description}</td>
+                        <td>{(movie.create_at)}</td>
+                        <td>
+                            {
+                                <>
+                                
+                                    {(movie.type_movie === "phimbo") ? <Button onClick={handleAddNewEpisode} variant="info">Thêm tập mới</Button> : <></>}
 
-                            </td>
-                            <td>{movie.views}</td>
-                            <td>{movie.description}</td>
-                            <td>{(movie.create_at)}</td>
-                            <td>
-                                {
-                                    <>
+                                    <Button as="button" onCLick={handleUpdateMovie} style={{ margin: "20px 0 20px 0"}} variant="warning">Chỉnh sửa</Button>
+                                    <Button as="button" onCLick={handleDeleteMovie} variant="danger">Xóa</Button>
+                                </>
+                            }
 
-                                        {(movie.type_movie === "phimbo") ? <Button onClick={handleAddNewEpisode} variant="info">Thêm tập mới</Button> : <></>}
-                                        <Button as="button" onCLick={handleUpdateMovie} style={{ margin: "20px 0 20px 0" }} variant="warning">Chỉnh sửa</Button>
-                                        <Button as="button" onCLick={handleDeleteMovie} variant="danger">Xóa</Button>
-                                    </>
-                                }
+                        </td>
 
-                            </td>
+                    </tr>
+                
+                )
+            }
 
-                        </tr>
-
-                    )
-                }
-
-            </tbody>
-        </Table>
+        </tbody>
+      </Table>
     );
 }
 
